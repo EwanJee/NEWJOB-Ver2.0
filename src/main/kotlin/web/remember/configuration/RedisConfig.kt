251dc704c5a23cb26e6ee08a598ac8a073469ef0
Ewan Jee <830jiforcareer@gmail.com>
2025-03-17 22:40:07 +0900

    feat: Swagger, Redis Config

---
+++ src/main/kotlin/web/remember/configuration/RedisConfig.kt
@@ -0,0 +1,30 @@
+package web.remember.configuration
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+class RedisConfig {
+    @Value("\${spring.data.redis.host}")
+    lateinit var host: String
+
+    @Value("\${spring.data.redis.port}")
+    lateinit var port: String
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(host, port.toInt())
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*, *> =
+        RedisTemplate<Any, Any>().apply {
+            this.connectionFactory = redisConnectionFactory()
+
+            // // "\xac\xed\x00" 같은 불필요한 해시값을 보지 않기 위해 serializer 설정
+            this.keySerializer = StringRedisSerializer()
+            this.hashKeySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+        }
+}
---
+++ src/main/kotlin/web/remember/configuration/SwaggerConfig.kt
@@ -0,0 +1,11 @@
+package web.remember.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.ForwardedHeaderFilter
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun forwardHeadFilter(): ForwardedHeaderFilter = ForwardedHeaderFilter()
+}

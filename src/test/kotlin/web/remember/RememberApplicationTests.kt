4f190203fe88c9d27b2ea3bd174e975f82921d8c
Ewan Jee <830jiforcareer@gmail.com>
2025-03-17 22:40:39 +0900

    init

---
+++ src/test/kotlin/web/remember/RememberApplicationTests.kt
@@ -0,0 +1,13 @@
+package web.remember
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RememberApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

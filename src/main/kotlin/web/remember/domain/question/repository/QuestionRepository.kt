dc2f30342fac00c8c4eb9f63883048b260360829
Ewan Jee <830jiforcareer@gmail.com>
2025-03-18 20:26:00 +0900

    feat : 필드값 ENUM 으로 변경

--- src/main/kotlin/web/remember/domain/question/repository/QuestionRepository.kt
+++ src/main/kotlin/web/remember/domain/question/repository/QuestionRepository.kt
@@ -3,6 +3,9 @@ package web.remember.domain.question.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import web.remember.domain.question.entity.Question
+import web.remember.domain.question.entity.Type
 
 @Repository
-interface QuestionRepository : JpaRepository<Question, Long>
+interface QuestionRepository : JpaRepository<Question, Long> {
+    fun findByType(type: Type): List<Question>
+}

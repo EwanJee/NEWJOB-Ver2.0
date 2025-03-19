0e5066ece4973c48b590c4d9de0a46341928083a
Ewan Jee <830jiforcareer@gmail.com>
2025-03-20 01:06:50 +0900

    feat : pdf 생성 컨트롤러 추가

---
+++ src/main/kotlin/web/remember/domain/test/application/pdf/PdfController.kt
@@ -0,0 +1,24 @@
+package web.remember.domain.test.application.pdf
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import web.remember.domain.test.presentation.pdf.PdfService
+
+@RestController
+@RequestMapping("/api/v1/pdf")
+class PdfController(
+    private val pdfService: PdfService,
+) {
+    @PostMapping()
+    fun createPdf(
+        name: String,
+        data: MutableMap<String, String>,
+        file: MultipartFile,
+    ): ResponseEntity<ByteArray> {
+        val pdf: ByteArray = pdfService.makeCareerPdf(name, data, file)
+        return ResponseEntity.ok(pdf)
+    }
+}

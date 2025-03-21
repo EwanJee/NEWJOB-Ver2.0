cbef13658cdfcbcdf54ea85ff40cfa9f5da02111
Ewan Jee <830jiforcareer@gmail.com>
2025-03-21 19:51:36 +0900

    feat: MultiPart 제대로 받기 위해 변경

--- src/main/kotlin/web/remember/domain/question/presentation/QuestionController.kt
+++ src/main/kotlin/web/remember/domain/question/presentation/QuestionController.kt
@@ -1,8 +1,10 @@
 package web.remember.domain.question.presentation
 
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import web.remember.domain.question.application.QuestionService
@@ -14,14 +16,16 @@ class QuestionController(
     private val questionService: QuestionService,
     private val careerService: CareerService,
 ) {
-    @PostMapping()
-    fun create(file: MultipartFile): ResponseEntity<Long> {
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun create(
+        @RequestPart("file") file: MultipartFile,
+    ): ResponseEntity<Long> {
         val questionId = questionService.create(file)
         return ResponseEntity.ok(questionId)
     }
 
     @PostMapping("/career")
-    fun makeCareerQuestion(): ResponseEntity<Map<String, List<String>>> {
+    fun makeCareerQuestion(): ResponseEntity<Map<String, Map<String, String>>> {
         val question = careerService.makeQuestion()
         return ResponseEntity.ok(question)
     }

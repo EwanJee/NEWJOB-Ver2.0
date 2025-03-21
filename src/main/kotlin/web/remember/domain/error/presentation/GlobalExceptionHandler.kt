9f3f1ba5ee5a2aaad43a7649117c91e00547c29f
Ewan Jee <830jiforcareer@gmail.com>
2025-03-21 19:47:22 +0900

    feat: ErrorMessage 구체화. status, timestamp 추가

--- src/main/kotlin/web/remember/domain/error/presentation/GlobalExceptionHandler.kt
+++ src/main/kotlin/web/remember/domain/error/presentation/GlobalExceptionHandler.kt
@@ -1,14 +1,24 @@
 package web.remember.domain.error.presentation
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import web.remember.domain.error.CustomException
+import java.time.LocalDateTime
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(CustomException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleCustomException(ex: CustomException): Map<String, String> = mapOf("error" to ex.message)
+    fun handleCustomException(ex: CustomException): ResponseEntity<Map<String, String>> {
+        val response =
+            mapOf(
+                "error" to ex.message,
+                "status" to HttpStatus.BAD_REQUEST.toString(),
+                "timestamp" to LocalDateTime.now().toString(),
+            )
+        return ResponseEntity.badRequest().body(response)
+    }
 }

d04c84a554069fb05195f89f75269553986db8af
Ewan Jee <830jiforcareer@gmail.com>
2025-03-23 19:39:26 +0900

    refactor : 디렉토리 변경

--- src/main/kotlin/web/remember/domain/test/application/dto/RequestCreateCareerTestDto.kt
+++
@@ -1,6 +0,0 @@
-package web.remember.domain.test.application.dto
-
-data class RequestCreateCareerTestDto(
-    val questionMap: Map<String, Map<String, String>>,
-    val memberId: String,
-)
--- src/main/kotlin/web/remember/domain/test/dto/RequestScoreUpdateDto.kt
+++
@@ -1,7 +0,0 @@
-package web.remember.domain.test.dto
-
-data class RequestScoreUpdateDto(
-    val memberId: String,
-    val testId: String,
-    val scoreMap: Map<String, Int>,
-)

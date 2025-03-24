80cd5340d405a3c3a0da310b6bf2afef57dae514
Ewan Jee <830jiforcareer@gmail.com>
2025-03-24 21:54:59 +0900

    feat : Pdf 세션 설정

--- src/main/kotlin/web/remember/domain/test/presentation/pdf/PdfCareerController.kt
+++ src/main/kotlin/web/remember/domain/test/presentation/pdf/PdfCareerController.kt
@@ -2,11 +2,13 @@
 
 package web.remember.domain.test.presentation.pdf
 
+import jakarta.servlet.http.HttpSession
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import web.remember.domain.error.CustomException
 import web.remember.domain.member.application.MemberService
 import web.remember.domain.test.application.career.CareerTestService
 import web.remember.domain.test.application.pdf.PdfService
@@ -20,16 +22,35 @@ class PdfCareerController(
 ) {
     @PostMapping("/create", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun createPdf(
-        @RequestParam("memberId") memberId: String,
-        @RequestParam("testId") testId: String,
+        @SessionAttribute("memberId", required = false) memberId: String?,
+        @SessionAttribute("testId", required = false) testId: String?,
         @RequestPart("file") file: MultipartFile,
+        httpSession: HttpSession,
     ): ResponseEntity<ByteArray> {
+        if (memberId == null || testId == null) {
+            throw CustomException("세션 정보가 없습니다. 로그인 후, 다시 실행해주세요")
+        }
+
         val dto = memberService.findNameAndIndustryById(memberId)
         val data = careerTestService.findDataById(testId)
         val pdf: ByteArray = pdfService.makeCareerPdf(dto.name, data, file)
+        httpSession.setAttribute("pdf", pdf)
         return ResponseEntity
             .ok()
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=result.pdf")
+            .contentType(MediaType.APPLICATION_PDF)
+            .body(pdf)
+    }
+
+    @GetMapping("")
+    fun viewPdf(
+        @SessionAttribute("pdf", required = false) pdf: ByteArray?,
+    ): ResponseEntity<ByteArray> {
+        if (pdf == null) {
+            throw CustomException("PDF 파일이 없습니다.")
+        }
+        return ResponseEntity
+            .ok()
             .contentType(MediaType.APPLICATION_PDF)
             .body(pdf)
     }

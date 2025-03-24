49cb301f1b7e27d06866a80a78fe752b7ac2b24f
Ewan Jee <830jiforcareer@gmail.com>
2025-03-24 16:35:27 +0900

    feat : 멤버 레포에 jdsl 반영

--- src/main/kotlin/web/remember/domain/member/application/MemberService.kt
+++ src/main/kotlin/web/remember/domain/member/application/MemberService.kt
@@ -2,9 +2,12 @@ package web.remember.domain.member.application
 
 import web.remember.domain.member.dto.RequestCreateMemberDto
 import web.remember.domain.member.dto.ResponseCreateMemberDto
+import web.remember.domain.member.repository.jdsl.dto.ResponseNameAndIndustry
 
 interface MemberService {
     fun create(member: RequestCreateMemberDto): ResponseCreateMemberDto
 
     fun existsById(id: String): Boolean
+
+    fun findNameAndIndustryById(id: String): ResponseNameAndIndustry
 }

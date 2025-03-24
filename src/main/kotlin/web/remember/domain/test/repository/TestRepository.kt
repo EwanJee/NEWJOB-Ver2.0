68128923fe158aa132ff860c4ae0ce247085a6b8
Ewan Jee <830jiforcareer@gmail.com>
2025-03-24 16:37:37 +0900

    feat : test의 data 조회 JPQL 추가

--- src/main/kotlin/web/remember/domain/test/repository/TestRepository.kt
+++ src/main/kotlin/web/remember/domain/test/repository/TestRepository.kt
@@ -1,8 +1,12 @@
 package web.remember.domain.test.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import web.remember.domain.test.entity.Test
 
 @Repository
-interface TestRepository : JpaRepository<Test, Long>
+interface TestRepository : JpaRepository<Test, Long> {
+    @Query("SELECT t.data FROM Test t WHERE t.id = :testId")
+    fun findDataById(testId: Long): MutableMap<String, String>?
+}

7e411801c85401ab227d17d90cbea241446afdca
Ewan Jee <830jiforcareer@gmail.com>
2025-03-24 23:05:26 +0900

    refactor : 지금은 안쓰므로 주석

--- src/main/kotlin/web/remember/configuration/KakaoConfig.kt
+++ src/main/kotlin/web/remember/configuration/KakaoConfig.kt
@@ -1,19 +1,14 @@
 package web.remember.configuration
 
-import net.nurigo.sdk.NurigoApp
-import net.nurigo.sdk.message.service.DefaultMessageService
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
+// @Configuration
 class KakaoConfig {
-//    @Value("\${kakao.api.key}")
+    //    @Value("\${kakao.api.key}")
     private var kakaoAPiKEy: String? = null
 
-//    @Value("\${kakao.api.secret}")
+    //    @Value("\${kakao.api.secret}")
     private var kakaoApiSecret: String? = null
 
     //    fun defaultMessageService(): DefaultMessageService = NurigoApp.initialize(kakaoAPiKEy, kakaoApiSecret, "https://api.coolsms.co.kr")
-    @Bean
-    fun defaultMessageService(): DefaultMessageService = NurigoApp.initialize("kakaoAPiKEy", "", "https://api.coolsms.co.kr")
+//    @Bean
+//    fun defaultMessageService(): DefaultMessageService = NurigoApp.initialize("kakaoAPiKEy", "", "https://api.coolsms.co.kr")
 }

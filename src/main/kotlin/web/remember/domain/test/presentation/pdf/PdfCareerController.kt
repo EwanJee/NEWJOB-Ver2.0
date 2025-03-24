885bcc050289290ac99442c425ec4f2312443e4d
Ewan Jee <830jiforcareer@gmail.com>
2025-03-24 16:36:02 +0900

    refactor : curl 매핑 오류로 인해서 param 요청 방식으로 변경

--- src/main/kotlin/web/remember/domain/test/presentation/pdf/PdfCareerController.kt
+++ src/main/kotlin/web/remember/domain/test/presentation/pdf/PdfCareerController.kt
@@ -7,21 +7,31 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import web.remember.domain.member.application.MemberService
+import web.remember.domain.test.application.career.CareerTestService
 import web.remember.domain.test.application.pdf.PdfService
-import web.remember.domain.test.presentation.pdf.dto.RequestCreatePdfDto
 
 @RestController
-@RequestMapping("/api/v1/pdf")
-class PdfController(
+@RequestMapping("/api/v1/pdf/career")
+class PdfCareerController(
     private val pdfService: PdfService,
+    private val memberService: MemberService,
+    private val careerTestService: CareerTestService,
 ) {
-    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @PostMapping("/create", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun createPdf(
-        @RequestBody dto: RequestCreatePdfDto,
+        @RequestParam("memberId") memberId: String,
+        @RequestParam("testId") testId: String,
         @RequestPart("file") file: MultipartFile,
     ): ResponseEntity<ByteArray> {
-        val pdf: ByteArray = pdfService.makeCareerPdf(dto.name, dto.data, file)
-        return ResponseEntity.ok(pdf)
+        val dto = memberService.findNameAndIndustryById(memberId)
+        val data = careerTestService.findDataById(testId)
+        val pdf: ByteArray = pdfService.makeCareerPdf(dto.name, data, file)
+        return ResponseEntity
+            .ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
+            .contentType(MediaType.APPLICATION_PDF)
+            .body(pdf)
     }
 
     @PostMapping("/test", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])

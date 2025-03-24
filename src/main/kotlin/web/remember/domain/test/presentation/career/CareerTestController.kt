05d5eeea9178abb23c0c99511a2163359a387dfd
Ewan Jee <830jiforcareer@gmail.com>
2025-03-24 21:54:47 +0900

    feat : Career Test 세션 설정

--- src/main/kotlin/web/remember/domain/test/presentation/career/CareerTestController.kt
+++ src/main/kotlin/web/remember/domain/test/presentation/career/CareerTestController.kt
@@ -1,5 +1,6 @@
 package web.remember.domain.test.presentation.career
 
+import jakarta.servlet.http.HttpSession
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -7,7 +8,6 @@ import web.remember.domain.error.CustomException
 import web.remember.domain.member.application.MemberService
 import web.remember.domain.test.application.career.CareerTestService
 import web.remember.domain.test.presentation.career.dto.RequestCreateCareerTestDto
-import web.remember.domain.test.presentation.career.dto.RequestFinishTest
 import web.remember.domain.test.presentation.career.dto.RequestScoreUpdateDto
 import web.remember.domain.test.presentation.career.dto.RequestUpdateCareerAge
 
@@ -20,32 +20,50 @@ class CareerTestController(
     @PostMapping()
     fun startTest(
         @RequestBody @Valid dto: RequestCreateCareerTestDto,
+        @SessionAttribute("memberId", required = false) memberId: String?,
+        httpSession: HttpSession,
     ): ResponseEntity<String> {
-        if (!memberService.existsById(dto.memberId)) {
+        if (memberId == null) {
+            throw CustomException("로그인이 필요합니다.")
+        }
+        if (!memberService.existsById(memberId)) {
             throw CustomException("존재하지 않는 회원입니다")
         }
-        val testId = careerTestService.startTest(dto.questionMap, dto.memberId)
+        val testId = careerTestService.startTest(dto.questionMap, memberId)
+        httpSession.setAttribute("testId", testId)
         return ResponseEntity.ok(testId)
     }
 
     @PutMapping("/score")
     fun updateScore(
         @RequestBody @Valid dto: RequestScoreUpdateDto,
+        @SessionAttribute("memberId", required = false) memberId: String?,
+        @SessionAttribute("testId", required = false) testId: String?,
     ) {
+        if (memberId == null || testId == null) {
+            throw CustomException("로그인이 필요합니다.")
+        }
+        dto.update(memberId, testId)
         careerTestService.updateScore(dto)
     }
 
     @PutMapping("/age")
     fun updateCareerAge(
         @RequestBody @Valid dto: RequestUpdateCareerAge,
+        @SessionAttribute("memberId", required = false) memberId: String?,
+        @SessionAttribute("testId", required = false) testId: String?,
     ) {
-        careerTestService.updateCareerAge(dto.testId, dto.memberId, dto.age)
+        if (memberId == null || testId == null) {
+            throw CustomException("로그인이 필요합니다.")
+        }
+        careerTestService.updateCareerAge(testId, memberId, dto.age)
     }
 
     @PutMapping("/finish")
     fun finishTest(
-        @RequestBody @Valid dto: RequestFinishTest,
+        @SessionAttribute("memberId") memberId: String,
+        @SessionAttribute("testId") testId: String,
     ) {
-        careerTestService.finishTest(dto.memberId, dto.testId)
+        careerTestService.finishTest(memberId, testId)
     }
 }

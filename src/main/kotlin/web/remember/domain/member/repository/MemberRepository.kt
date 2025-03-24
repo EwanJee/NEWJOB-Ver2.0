60106d9b1c2c831fa85ec6b10c3e3959d5bac27b
Ewan Jee <830jiforcareer@gmail.com>
2025-03-24 16:35:23 +0900

    feat : 멤버 레포에 jdsl 반영

--- src/main/kotlin/web/remember/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/web/remember/domain/member/repository/MemberRepository.kt
@@ -3,8 +3,11 @@ package web.remember.domain.member.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import web.remember.domain.member.entity.Member
+import web.remember.domain.member.repository.jdsl.CustomizedMemberRepository
 
 @Repository
-interface MemberRepository : JpaRepository<Member, String> {
+interface MemberRepository :
+    JpaRepository<Member, String>,
+    CustomizedMemberRepository {
     fun existsByPhoneNumber(phoneNumber: String): Boolean
 }

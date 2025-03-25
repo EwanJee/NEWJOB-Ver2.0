1dfc65fa253f880b73f48fae88fd463902d2ce61
Ewan Jee <830jiforcareer@gmail.com>
2025-03-26 01:33:37 +0900

    feat : 배포 환경 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 }
 
 dependencies {
+//    implementation("software.amazon.awssdk:s3:2.31.6")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter:3.1.1")
     // 카카오톡 알림톡 API
     implementation("net.nurigo:sdk:4.3.2")
     // Kotlin JDSL
@@ -40,7 +42,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     // 지금은 안쓰므로 주석처리
 //    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.3.9")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -52,9 +54,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     // cache
-    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-cache:3.3.9")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.session:spring-session-data-redis:3.3.6")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")

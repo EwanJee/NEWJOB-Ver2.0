bde0398140dce7a640e4ef5e296a645ec7b5b76b
Ewan Jee <830jiforcareer@gmail.com>
2025-03-26 18:06:09 +0900

    refactor : docker namespace 변경

--- automation.sh
+++ automation.sh
@@ -4,7 +4,7 @@
 docker-compose -f compose.prod.yaml build
 
 # 이미지 태그 지정 (이미지 이름과 Docker Hub 저장소 이름 수정)
-docker tag api ewanjeedocker/api:latest
+docker tag api ewanjee/api:latest
 
 # Docker Hub로 이미지 푸시
-docker push ewanjeedocker/api:latest
+docker push ewanjee/api:latest

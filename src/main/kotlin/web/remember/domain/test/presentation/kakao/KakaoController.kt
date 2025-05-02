2e49a6f6dd3cffb95e38f78346d1a3cf7f9bd2ab
Ewan Jee <830jiforcareer@gmail.com>
2025-05-02 23:58:56 +0900

    feat: 메세지 보내기 기능 추가

--- src/main/kotlin/web/remember/domain/test/presentation/kakao/KakaoController.kt
+++ src/main/kotlin/web/remember/domain/test/presentation/kakao/KakaoController.kt
@@ -1,35 +1,49 @@
 package web.remember.domain.test.presentation.kakao
 
+import org.springframework.http.ResponseEntity
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 
 @RestController
 @RequestMapping("/api/v1/kakao")
 class KakaoController(
-//    private final val kakaoService: DefaultMessageService,
+    private val webClientBuilder: WebClient.Builder,
 ) {
-//    @PostMapping("/send")
-//    fun sendKakaoMessage(): SingleMessageSentResponse? {
-//        // 등록하신 카카오 비즈니스 채널의 pfId를 입력해주세요.
-//        // 등록하신 카카오 알림톡 템플릿의 templateId를 입력해주세요.
-//        val kakaoOption =
-//            KakaoOption(
-//                pfId = "pfId 입력",
-//                templateId = "templateId 입력",
-//                // disableSms를 true로 설정하실 경우 문자로 대체발송 되지 않습니다.
-//                disableSms = true,
-//                // 알림톡 템플릿 내에 #{변수} 형태가 존재할 경우 variables를 설정해주세요.
-//                // variables = variables,
-//            )
-//
-//        // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
-//        val message =
-//            Message(
-//                from = "01094118219",
-//                to = "수신번호 입력",
-//                kakaoOptions = kakaoOption,
-//            )
-//        val response = kakaoService.sendOne(SingleMessageSendingRequest(message))
-//        return response
-//    }
+    @PostMapping("/sendPdf")
+    fun sendPdf(
+        @RegisteredOAuth2AuthorizedClient("kakao") authorizedClient: OAuth2AuthorizedClient,
+        @RequestBody pdfUrl: String,
+    ): ResponseEntity<String> {
+        val accessToken = authorizedClient.accessToken.tokenValue
+        val webClient: WebClient = webClientBuilder.build()
+        val json =
+            """
+{
+  "object_type": "text",
+  "text": "PDF 파일이 생성되었습니다. 아래 링크를 클릭하여 다운로드하세요.",
+  "link": {
+    "web_url": "$pdfUrl",
+    "mobile_web_url": "$pdfUrl"
+  }
+}
+            """.trimIndent()
+
+        val response =
+            webClient
+                .post()
+                .uri("https://kapi.kakao.com/v2/api/talk/memo/default/send")
+                .header("Authorization", "Bearer $accessToken")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .bodyValue("template_object=$json")
+                .retrieve()
+                .bodyToMono(String::class.java)
+                .block()
+
+        return ResponseEntity.ok(response ?: "No Response")
+    }
 }

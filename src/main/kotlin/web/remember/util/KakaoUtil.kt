56a1f4ebc7f75511dcb508468c5e613009b2f7a6
Ewan Jee <830jiforcareer@gmail.com>
2025-05-03 21:21:25 +0900

    refactor : @Value 필드 가져오기 방법 변경

--- src/main/kotlin/web/remember/util/KakaoUtil.kt
+++ src/main/kotlin/web/remember/util/KakaoUtil.kt
@@ -18,13 +18,13 @@ class KakaoUtil(
     private val objectMapper = ObjectMapper()
 
     @Value("\${spring.security.oauth2.client.registration.kakao.client-id}")
-    var clientId: String = ""
+    private lateinit var clientId: String
 
     @Value("\${spring.security.oauth2.client.registration.kakao.redirect-uri}")
-    var redirectUri: String = ""
+    private lateinit var redirectUri: String
 
     @Value("\${spring.security.oauth2.client.registration.kakao.client-secret}")
-    var clientSecret: String = ""
+    private lateinit var clientSecret: String
 
     fun requestToken(accessCode: String): ResponseTokenDto {
         val webClient: WebClient =

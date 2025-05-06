1dfa73a1ddd329ae8480b111882d958787039d11
Ewan Jee <830jiforcareer@gmail.com>
2025-05-07 01:21:42 +0900

    refactor : 안쓰는 컨트롤러 메서드 임시 주석 처리

--- src/main/kotlin/web/remember/domain/member/presentation/MemberController.kt
+++ src/main/kotlin/web/remember/domain/member/presentation/MemberController.kt
@@ -2,22 +2,49 @@
 
 package web.remember.domain.member.presentation
 
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletResponse
 import jakarta.servlet.http.HttpSession
 import jakarta.validation.Valid
 import org.slf4j.Logger
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import web.remember.domain.member.application.MemberRedisService
 import web.remember.domain.member.application.MemberService
-import web.remember.domain.member.dto.RequestAuthLoginDto
 import web.remember.domain.member.dto.RequestCreateMemberDto
 import web.remember.domain.member.dto.ResponseCreateMemberDto
+import web.remember.util.JwtUtil
+import web.remember.util.KakaoUtil
 
 @RequestMapping("/api/v1/members")
 @RestController
 class MemberController(
     private val memberService: MemberService,
+    private val kakaoUtil: KakaoUtil,
+    private val jwtUtil: JwtUtil,
+    private val memberRedisService: MemberRedisService,
 ) {
     val logger: Logger = org.slf4j.LoggerFactory.getLogger(MemberController::class.java)
+
+    @GetMapping("/logout")
+    fun logout(
+        @CookieValue("jwt") jwt: String,
+        response: HttpServletResponse,
+    ): ResponseEntity<Void> {
+        val claims: MutableMap<String, Any> = jwtUtil.parseClaims(jwt)
+        val kakaoId = claims["kakaoId"] as String
+        if (claims["kakaoId"] == null) {
+            return ResponseEntity.badRequest().build()
+        }
+        memberRedisService.evictToken(kakaoId)
+        val expiredCookie =
+            Cookie("jwt", null).apply {
+                maxAge = 0
+                path = "/"
+            }
+        response.addCookie(expiredCookie)
+        return ResponseEntity.ok().build()
+    }
 
     @PostMapping()
     fun approach(
@@ -29,14 +56,13 @@ class MemberController(
         return ResponseEntity.ok(responseDto)
     }
 
-    @PostMapping("/login")
-    fun authLogin(
-        @RequestBody dto: RequestAuthLoginDto,
-        httpSession: HttpSession,
-    ): ResponseEntity<ResponseCreateMemberDto> {
-        val responseDto = memberService.create(dto)
-        logger.info("Response DTO: $responseDto") // DTO의 내용을 로그로 출력
-        httpSession.setAttribute("memberId", responseDto.id)
-        return ResponseEntity.ok(responseDto)
-    }
+//    @PostMapping("/login")
+//    fun authLogin(
+//        @RequestBody dto: RequestAuthLoginDto,
+//        httpSession: HttpSession,
+//    ): ResponseEntity<ResponseCreateMemberDto> {
+//        val responseDto = memberService.create(dto)
+//        httpSession.setAttribute("memberId", responseDto.id)
+//        return ResponseEntity.ok(responseDto)
+//    }
 }

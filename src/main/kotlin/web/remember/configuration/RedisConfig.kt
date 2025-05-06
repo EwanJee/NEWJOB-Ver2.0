6571d215fb1413b4bd1b72edf14d808505b90a8e
Ewan Jee <830jiforcareer@gmail.com>
2025-05-07 01:16:53 +0900

    refactor : JWT사용으로 인한 레디스 세션 무효화

--- src/main/kotlin/web/remember/configuration/RedisConfig.kt
+++ src/main/kotlin/web/remember/configuration/RedisConfig.kt
@@ -13,12 +13,10 @@ import org.springframework.data.redis.connection.lettuce.LettuceClientConfigurat
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 import java.time.Duration
 
 @Configuration
 @EnableCaching
-@EnableRedisHttpSession
 class RedisConfig {
     @Value("\${spring.data.redis.host}")
     lateinit var host: String

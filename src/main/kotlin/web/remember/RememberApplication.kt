f5d22e6ea54be8fc83b8a91e6e2f5c49c7114411
Ewan Jee <830jiforcareer@gmail.com>
2025-05-26 19:37:09 +0900

    refactor : Jpa Auditing 추가

--- src/main/kotlin/web/remember/RememberApplication.kt
+++ src/main/kotlin/web/remember/RememberApplication.kt
@@ -2,8 +2,10 @@ package web.remember
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class RememberApplication
 
 fun main(args: Array<String>) {

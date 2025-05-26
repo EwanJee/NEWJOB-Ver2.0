5875af6a3bc89b15f94824e4587dd42b4a124eda
Ewan Jee <830jiforcareer@gmail.com>
2025-05-26 20:18:48 +0900

    refactor : rabbitmq 도커 같은 네트워크 안에 있게 설정

--- automation.sh
+++ automation.sh
@@ -2,7 +2,6 @@
 
 # Docker Compose 빌드
 docker-compose -f compose.prod.yaml build
-docker-compose -f compose.amqp.yaml build
 
 # 이미지 태그 지정 (이미지 이름과 Docker Hub 저장소 이름 수정)
 docker tag api ewanjee/api:latest
